1971f9a46900f8c5382b48b50b4519d9192887c6
Nishant <sharmanishant6500@gmail.com>
2024-11-20 00:16:21 +0530

    added slugify in index.js file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 const replaceTemplate = require('./modules/replaceTemplate');
-
+const slugify = require('slugify');
 
 ////////////////////////////////////////////////////////
 ///// FILE
@@ -40,6 +40,10 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`,'utf-8',);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8',);
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, {lower:true}));
+console.log(slugs);
+
 
 const server = http.createServer((req,res) => {
     const { query, pathname } = url.parse(req.url, true);
